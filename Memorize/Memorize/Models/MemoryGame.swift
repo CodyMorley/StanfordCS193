463c8d38d104f2d9e23d43de1b1a9ec4b8c6aa74
Morley <codymorleyinc@gmail.com>
2020-12-07 18:36:39 -0800

    Lecture 2: MVVM and the Swift Type System
    End of Lecture

---
+++ Memorize/Memorize/Models/MemoryGame.swift
@@ -0,0 +1,42 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Cody Morley on 12/7/20.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    //MARK: - Types
+    struct Card: Identifiable {
+        //MARK: - Card Properties
+        var id: Int
+        var isFaceUp = true
+        var isMatched = false
+        var content: CardContent
+    }
+    
+    
+    //MARK: - Properties
+    var cards: [Card]
+    
+    
+    //MARK: - Init
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = [Card]()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(id: pairIndex * 2,
+                              content: content))
+            cards.append(Card(id: pairIndex * 2 + 1,
+                              content: content))
+        }
+    }
+    
+    
+    //MARK: - Methods
+    func choose(card: Card) {
+        print("Card chosen: \(card)")
+    }
+}
---
+++ Memorize/Memorize/View Models/EmojiMemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Cody Morley on 12/7/20.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    //MARK: - Static Methods
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["🎅🏼", "🎄", "🦌"]
+        
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    
+    //MARK: - Properties
+    private var game: MemoryGame<String> = createMemoryGame()
+    ///Access to the model.
+    var cards: Array<MemoryGame<String>.Card> {
+        game.cards
+    }
+    
+    
+    //MARK: - Intents
+    func choose(card: MemoryGame<String>.Card) {
+        game.choose(card: card)
+    }
+}
