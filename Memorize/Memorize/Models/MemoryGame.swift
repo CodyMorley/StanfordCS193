83bbabb826a8e9cd328e12e54175b0ed2fc53488
Morley <codymorleyinc@gmail.com>
2020-12-07 19:32:00 -0800

    Lecture 2: Homework
    Finished Lecture work
    Finished Shuffle

--- Memorize/Memorize/Models/MemoryGame.swift
+++ Memorize/Memorize/Models/MemoryGame.swift
@@ -27,11 +27,13 @@ struct MemoryGame<CardContent> {
         cards = [Card]()
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = cardContentFactory(pairIndex)
+            
             cards.append(Card(id: pairIndex * 2,
                               content: content))
             cards.append(Card(id: pairIndex * 2 + 1,
                               content: content))
         }
+        cards.shuffle()
     }
     
     
